727b795c38ca472256e99e03d9e92f8f97163781
codeus-ex-machina <thegreatjordan@gmx.de>
2022-11-07 18:49:33 +0100

    audio files added, playing on click

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,36 @@ import DrumPad from "./Components.js";
 
 function App() {
   /* DRUMPADS ******************************************/
+  const audioSourceList = [
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  ];
   const drumpads = (
     <>
       {[
-        ["q", "Heater 1"],
-        ["w", "Heater 2"],
-        ["e", "Heater 3"],
-        ["a", "Heater 4"],
-        ["s", "Clap"],
-        ["d", "Open-HH"],
-        ["y", "Kick-n'-Hat"],
-        ["x", "Kick"],
-        ["c", "Closed-HH"],
-      ].map((el) => (
-        <DrumPad key={el[0]} keyboardKey={el[0]} soundType={el[1]} />
+        ["q", "Heater 1", ""],
+        ["w", "Heater 2", ""],
+        ["e", "Heater 3", ""],
+        ["a", "Heater 4", ""],
+        ["s", "Clap", ""],
+        ["d", "Open-HH", ""],
+        ["y", "Kick-n'-Hat", ""],
+        ["x", "Kick", ""],
+        ["c", "Closed-HH", ""],
+      ].map((el, index) => (
+        <DrumPad
+          key={el[0]}
+          keyboardKey={el[0].toUpperCase()}
+          soundType={el[1]}
+          audioSource={audioSourceList[index]}
+        />
       ))}
     </>
   );
@@ -28,7 +44,6 @@ function App() {
         <div id="display">Display</div>
 
         {drumpads}
-        <DrumPad />
 
         <img src="https://images.pexels.com/photos/5650522/pexels-photo-5650522.jpeg?cs=srgb&dl=pexels-cottonbro-5650522.jpg&fm=jpg&_gl=1*hsjsxn*_ga*MTc5MDg4NDczMC4xNjQ3NDM5ODM4*_ga_8JE65Q40S6*MTY2NzMwMzU3Ni4yLjEuMTY2NzMwMzY0NS4wLjAuMA.." />
       </div>
--- src/Components.js
+++ src/Components.js
@@ -2,8 +2,17 @@ import PropTypes from "prop-types";
 
 export default function DrumPad(props) {
   return (
-    <div className="drum-pad" id={props.soundType}>
+    <div
+      className="drum-pad"
+      id={props.soundType}
+      onClick={() => document.getElementById(props.keyboardKey).play()}
+    >
       {props.keyboardKey}
+      <audio
+        classname="clip"
+        id={props.keyboardKey}
+        src={props.audioSource}
+      ></audio>
     </div>
   );
 }
@@ -11,7 +20,9 @@ export default function DrumPad(props) {
 DrumPad.propTypes = {
   soundType: PropTypes.string,
   keyboardKey: PropTypes.string,
+  audioSource: PropTypes.string.isRequired,
 };
+
 DrumPad.defaultProps = {
   soundType: "unspecified",
   keyboardKey: "click me!",
